2a4dd8219f0953da51e58dc4367d135af9ed1d39
Sagittta <js1verj@gmail.com>
2019-07-25 19:36:49 +0900

    Add function for Character Select

--- js/setCharacter.js
+++ js/setCharacter.js
@@ -2,3 +2,19 @@ window.console = window.console || function (t) { };
     if (document.location.search.match(/type=embed/gi)) {
       window.parent.postMessage("resize", "*");
     }
+
+var chracter = "";
+function select(){
+  var animal = document.getElementsByName("animal")[0].id;
+  alert(animal);
+
+  switch(animal) {
+    case "seal": character = "seal";
+    break;
+    case "turtle": character = "turtle";
+    break;
+    case "shrimp": character = "shrimp";
+    break;
+    default: break;
+  }
+}
